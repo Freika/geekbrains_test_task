a21d2330182ff60bc325c059e1446a1167db5f0b
Eugene Burmakin <iamfrey@gmail.com>
2020-04-13 18:16:49 +0300

    Use basic auth to protect admin part.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  http_basic_authenticate_with name: 'admin', password: 'admin', if: :admin_controller?
 
   def current_user
     if session[:user_id]
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     else
       @current_user = nil
     end
+  end
+
+  def admin_controller?
+    self.class < ActiveAdmin::BaseController
   end
 end
