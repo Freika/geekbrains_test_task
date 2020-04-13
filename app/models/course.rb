a74517f558ab36bbc54885f896b099c5a92b27e2
Eugene Burmakin <iamfrey@gmail.com>
2020-04-13 17:47:07 +0300

    Minor indentation fix.

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,6 @@ class Course < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
 
-  scope :nearest,   -> { joins(:groups).merge(Group.order(starts_on: :asc)) }
+  scope :nearest,  -> { joins(:groups).merge(Group.order(starts_on: :asc)) }
   scope :farthest, -> { joins(:groups).merge(Group.order(starts_on: :desc)) }
 end
